ffea4b028c298a7ebdbdc9a287765ec4e1dd06cb
gibrankasif <61095696+gibrankasif@users.noreply.github.com>
2021-02-22 19:36:52 +0530

    Added Buttons

---
+++ app/src/main/java/com/iit/mobile_cwk01/CarMakeActivity.java
@@ -0,0 +1,17 @@
+package com.iit.mobile_cwk01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarMakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_make);
+        this.setTitle("My Title");
+
+    }
+
+}
